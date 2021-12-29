89d67ec09c83d0f0d9b1d3fc865b145d647ffc0f
Initram89 <goran_sb@yahoo.com>
2021-12-29 20:39:21 +0100

    Add components and prices

--- src/pastaBar/PastaBar.java
+++ src/pastaBar/PastaBar.java
@@ -4,7 +4,10 @@ public class PastaBar {
 
 public static void main(String[] args) {
 		
-		
+	String[] components = {"Makarone", "Spagete", "Bolognese", "Curetina", "Govedja prsuta", "Slanina", "Piletina", "4 sira", "Dimljeni sir",
+			"Parmezan", "Pavlaka", "Pesto sos",	"Napolitana", "Povrce mix", "Pecurke", "Kutija"};
+	
+	int[] price = {50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20};
 		
 	}
 	
