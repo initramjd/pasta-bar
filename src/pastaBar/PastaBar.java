1fefbbda282957f4ec732541df17c606fa3be0fc
Initram89 <goran_sb@yahoo.com>
2021-12-29 20:41:12 +0100

    Add phone numbers

--- src/pastaBar/PastaBar.java
+++ src/pastaBar/PastaBar.java
@@ -8,6 +8,8 @@ public static void main(String[] args) {
 			"Parmezan", "Pavlaka", "Pesto sos",	"Napolitana", "Povrce mix", "Pecurke", "Kutija"};
 	
 	int[] price = {50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20};
+	
+	String[] phoneNumber = {"0631111111", "063222222", "063333333", "064444444", "065555555", "066666666"};
 		
 	}
 	
