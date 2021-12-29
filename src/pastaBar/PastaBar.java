4739eb1bd6811d109f530993fb59823edad0cd99
Initram89 <goran_sb@yahoo.com>
2021-12-29 21:24:36 +0100

    Deleted superfluous Code

--- src/pastaBar/PastaBar.java
+++ src/pastaBar/PastaBar.java
@@ -1,34 +1,44 @@
 package pastaBar;
 
+import java.util.Scanner;
+
 public class PastaBar {
 
 	public static void main(String[] args) {
 
+		Scanner s = new Scanner(System.in);
+		
 		String[] components = { "Makarone", "Spagete", "Bolognese", "Curetina", "Govedja prsuta", "Slanina", "Piletina",
 				"4 sira", "Dimljeni sir", "Parmezan", "Pavlaka", "Pesto sos", "Napolitana", "Povrce mix", "Pecurke",
 				"Kutija" };
 
 		int[] price = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+		
 
+		String component = "";
 		String[] phoneNumbers = { "0631111111", "063222222", "063333333", "064444444", "065555555", "066666666" };
 		String phoneNumber;
 
 		wellcome();
 		menu();
-		addComponents();
+			
+		while (!isOrderFinish(component)) {
+		
+		addComponents();		
+			
+		System.out.print("Unesite vas broj telefona: ");
+		phoneNumber = s.next();
+			
 
+			
+			if (searchIndex(components, component) == -1) {				
+				System.out.println("Niste uneli odgovarajuci prilog! ");
+		
+		}
+		
 	}
-
-	public static int findIngredient(String[] ingredients, String ingredientName) {
-
-		return 0;
 	}
-
-	public static boolean isRegularCustomer(String[] phoneNumbers, String phone) {
-
-		return false;
-	}
-
+	
 	public static void wellcome() {
 
 		System.out.println("Izvolite !!");
@@ -36,25 +46,21 @@ public class PastaBar {
 	}
 
 	public static void addComponents() {
-
-		System.out.print("Izaberite sastojak za pastu: ");
-	}
+		System.out.print("Izaberite sastojak za pastu: ");	}
+	
+	public static void addPhoneNumber() {		
+		System.out.print("Unesite vas broj telefona: ");	}
 
 	public static boolean vipCheck(String[] phoneNumbers, String phoneNumber) {
-
 		for (int i = 0; i < phoneNumbers.length; i++) {
-
 			if (phoneNumber.equals(phoneNumbers[i])) {
-
 				return true;
 			}
 		}
-
 		return false;
 	}
 	
-	public static void menu() {
-		
+	public static void menu() {		
 		System.out.println("Sastojci i cene:\r\n" + 
 				"Makarone - 50 rsd\r\n" + 
 				"Spagete - 60 rsd\r\n" + 
@@ -72,6 +78,27 @@ public class PastaBar {
 				"Povrce mix – 50 rsd\r\n" + 
 				"Pecurke – 50 rsd\r\n" + 
 				"Kutija – 20 rsd\r\n");
+	}
+	
+	public static int searchIndex(String[] components, String component) {
+		int i = 0;
+		
+		while (i < components.length){
+			
+			if (component.equals(components[i])) {
+				
+				return i;
+			}
+			i++;
+		}
+		
+		return -1;
+	}
+	
+	public static boolean isOrderFinish(String component) {
+		
+		return (component.equals("Poruci"));
+		
 	}
 
 }
