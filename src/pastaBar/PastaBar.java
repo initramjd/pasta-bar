9bef290077f606b77021bb9ebd9eb9e7ede5f670
Initram89 <goran_sb@yahoo.com>
2021-12-29 20:31:33 +0100

    Init project

---
+++ src/pastaBar/PastaBar.java
@@ -0,0 +1,22 @@
+package pastaBar;
+
+public class PastaBar {
+
+public static void main(String[] args) {
+		
+		
+		
+	}
+	
+	public static int findIngredient(String[] ingredients, String ingredientName) {
+		
+		return 0;
+	}
+	
+	
+	public static boolean isRegularCustomer(String[] phoneNumbers, String phone) {
+		
+		return false;
+	}
+
+}
